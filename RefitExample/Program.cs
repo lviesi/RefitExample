e2ed56d1a4fabb232f686a638d907e30861b2bea
Lucas Viesi <lucasviesi@hotmail.com>
2021-03-31 12:44:41 -0300

    Implementacao de uma console application consumindo uma API de CEP utilizando Refit

---
+++ RefitExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefitExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

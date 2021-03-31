f2bd408376535b7ba1252cf1c1c2e1a6de2b447f
Lucas Viesi <lucasviesi@hotmail.com>
2021-03-31 12:48:30 -0300

    Implementacao de uma console application consumindo uma API de CEP utilizando Refit

--- RefitExample/Program.cs
+++ RefitExample/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Refit;
+using RefitExample.Services;
+using System;
+using System.Threading.Tasks;
 
 namespace RefitExample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var cepClient = RestService.For<ICepService>("https://viacep.com.br");
+
+                Console.WriteLine("Digite o seu CEP:");
+                string cep = Console.ReadLine();
+
+                Console.WriteLine($"\nConsultando dados do CEP {cep}...");
+
+                var address = await cepClient.GetAddressAsync(cep);
+
+                Console.Write($"\nLogradouro: {address.Logradouro}" +
+                    $"\nBairro: {address.Bairro}" +
+                    $"\nLocalidade: {address.Localidade}" +
+                    $"\nEstado: {address.Uf}" +
+                    $"\nDDD: {address.Ddd}" +
+                    $"\nCódigo Ibge: {address.Ibge}");
+
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine($"Ocorreu um erro ao consultar seu CEP!");
+                Console.ReadKey();
+            }            
         }
     }
 }
---
+++ RefitExample/Services/ICepService.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using RefitExample.Responses;
+using System.Threading.Tasks;
+
+namespace RefitExample.Services
+{
+    public interface ICepService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
